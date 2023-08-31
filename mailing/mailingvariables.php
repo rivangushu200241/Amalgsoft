5fa49e6fc6dbc22ca58de8a387eef010bacfae8c
AMALGSOFT <rivangushu200241>
2023-08-31 09:34:18 +0300

    first commit

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "smtp.gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = "rivangushu@gmail.com"; //sender
+    $mail_sender_password = "hvnknhcngdctcaig"; //sender
+    $mail_sender_name = "AMALGSOFT";
+?>
